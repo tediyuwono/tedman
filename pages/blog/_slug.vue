7169334246a2bb29188a76d11e80b04d99885316
Tedi <tedi@yuwono.or.id>
2020-08-24 08:53:46 +0700

    Update _slug.vue

--- pages/blog/_slug.vue
+++ pages/blog/_slug.vue
@@ -4,14 +4,6 @@
       <div class="elevate-cover">
         <div class="elevate-cover__textOffset">
           <div class="elevate-cover__left">
-            <nuxt-link :to="localePath('index')">
-              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 6 4" aria-hidden="true" style="width: 16px; transform: rotate(180deg);">
-                  <polygon fill="currentColor" points="0 2.33 4.72 2.33 3.53 3.53 4 4 6 2 4 0 3.53 0.47 4.72 1.67 0 1.67 0 2.33"/>
-              </svg>
-              {{ $t('comeBack') }}
-            </nuxt-link>
-          </div>
-          <div class="elevate-cover__left">
             <span class="blogSelected-year">{{ year }}</span>
             —
             <template v-if="trans">
